84922dc352423c9c70df7b0f9e9e2434c0185b0a
Aravind Sant <aravindsant563@gmail.com>
2024-10-25 16:41:22 +0530

    Some input Filed change

--- src/Component/Input.jsx
+++ src/Component/Input.jsx
@@ -7,7 +7,7 @@ const Input=({inputVal})=> {
     <div className=''>
       <input type="text" 
         className='border-4 border-black text-center 
-       content-center p-3 m-2  calc font-bold '
+       content-center p-3 mt-2  calc font-bold max-w-80 w-full  sm:w-1/2 md:w-1/3 lg:w-1/4z'
       placeholder='Enter number' 
       value={inputVal} readOnly />
       
